2bbbd8d135532f2d5c67235a88906ec88b3cf340
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-07-18 16:37:55 +0700

    chap14-follow-unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,27 @@ class User < ApplicationRecord
 
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
 
 
   def User.digest(string)
@@ -64,8 +85,13 @@ class User < ApplicationRecord
     end
 
     def feed
-      Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
     end
+
+
 /private method/
 
     private
