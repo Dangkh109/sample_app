7eac08ee84415de906e636eb5f38bfe129ff1a88
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-07-20 09:32:30 +0700

    mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,9 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  host = 'kieu.hai.dang@framgia.com' # Don't use this literally; use your local dev host instead
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-  
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
