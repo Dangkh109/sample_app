9fbdf391404326f34490b69927b2bda2b2ec0d01
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-07-24 22:53:02 +0700

    change 10-52

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page], :per_page => 10)
-    @comment = nil
+    @comment = Comment.new
   end
   def create
   	@user = User.new(user_params)
