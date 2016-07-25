2c0966127519855657372821b947faaa5125ac48
Kieu Hai Dang <kieu.hai.dang@framgia.com>
2016-07-25 09:27:55 +0700

    done add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,21 @@
 class CommentsController < ApplicationController
   def index
-    @micropost = Micropost.find_by(id: params[:micropost_id])
+    @micropost = Micropost.find_by(id: params[comment_params])
     @comments = @micropost.comments
   end
 
   def new
-    @micropost = Micropost.find_by(id: params[:micropost_id])
+    @micropost = Micropost.find_by(id: params[comment_params])
     @comment = @micropost.comments.build()
   end
 
   def create
-    @micropost = Micropost.find_by(id: params[:micropost_id])
-    @comment = @micropost.comments.build(params[:comment])
+    @comment = current_user.comments.build(comment_params)
     if @comment.save
       flash[:success] = "Comment created"
-      redirect_to micropost_comment_url(@micropost, @comment)
+      redirect_to root_url
     else
-      render 'new'
+      redirect_to root_url
     end
   end
 
@@ -26,8 +25,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find_by(id: params[:id])
     @comment.destroy
     flash[:success] = "Comment deleted"
     redirect_to root_url
+
+  end
+
+  private
+   def comment_params
+    params.require(:comment).permit(:content, :micropost_id)
   end
 end
+
